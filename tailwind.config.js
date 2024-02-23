cd4e02c5cc8fc5f3b3acbdfd28ec1932f29cfd74
rakesh <rakeshkumar748844@gmail.com>
2024-02-23 19:36:07 +0530

    fix-footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {
