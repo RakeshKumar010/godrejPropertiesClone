719a4218ec3f54323c54489ea5267491166f0a2d
rakesh <rakeshkumar748844@gmail.com>
2024-02-23 10:24:23 +0530

    'pagination-fix'

--- src/components/ExpHome.jsx
+++ src/components/ExpHome.jsx
@@ -5,9 +5,13 @@ import Pagination from "./Pagination";
 
 const ExpHome = () => {
   const [pageNo, setPageNo] = useState(0);
+  const [sPage,setSPage]=useState(0)
+  const[ePage,setEPage]=useState(9)
+  const totalItem =Homes.length;
+  
 
   useEffect(() => {
-    setPageNo(Math.ceil(Homes.length / 9));
+    setPageNo(Math.ceil(totalItem / 9));
     
   }, []);
 
@@ -17,7 +21,7 @@ const ExpHome = () => {
         <p className="text-lg font-semibold mb-5 text-nowrap">EXPLORE HOMES</p>
       </div>
       <div className=" mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
-        {Homes.map((value, index) => {
+        {Homes.slice(sPage,ePage).map((value, index) => {
           return (
             <>
               {index < 9 ? <ExpHomeCards value={value} /> : null}
@@ -25,8 +29,12 @@ const ExpHome = () => {
           );
         })}
       </div>
+      {sPage}
+      {ePage}
+      {pageNo}
+      {totalItem}
 
-      <Pagination pageNo={pageNo} />
+      <Pagination pageNo={pageNo} totalItem={totalItem} setSPage={setSPage} sPage={sPage} ePage={ePage} setEPage={setEPage}/>
     </div>
   );
 };
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
-const Pagination = ({ pageNo }) => {
+const Pagination = ({ pageNo, setSPage, setEPage, ePage, sPage,totalItem }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageChange = (newPage) => {
@@ -12,9 +12,17 @@ const Pagination = ({ pageNo }) => {
 
   return (
     <div className="flex items-center gap-5 justify-center select-none mt-5">
-      <FaAngleLeft 
-        className={`bg-gray-200 text-3xl p-2 rounded-full ${currentPage > 1 ? "text-gray-900" : "text-gray-400"} cursor-pointer`} 
-        onClick={() => handlePageChange(currentPage - 1)} 
+      <FaAngleLeft
+        className={`bg-gray-200 text-3xl p-2 rounded-full ${
+          currentPage > 1 ? "text-gray-900" : "text-gray-400"
+        } cursor-pointer`}
+        onClick={() => {
+          handlePageChange(currentPage - 1);
+          if (sPage > 0) {
+            setSPage(sPage - 9);
+            setEPage(ePage - 9);
+          }
+        }}
       />
       {[...Array(pageNo)].map((_, index) => {
         const page = index + 1;
@@ -22,16 +30,29 @@ const Pagination = ({ pageNo }) => {
           <span
             key={index}
             onClick={() => handlePageChange(page)}
-            className={`cursor-pointer ${page === currentPage ?
-               "text-white px-3 py-1 rounded-full bg-black" :"text-gray-500"}`}
+            className={`cursor-pointer ${
+              page === currentPage
+                ? "text-white px-3 py-1 rounded-full bg-black"
+                : "text-gray-500"
+            }`}
           >
             {page}
           </span>
         );
       })}
-      <FaAngleRight 
-        className={`bg-gray-200 text-3xl p-2 rounded-full ${currentPage < pageNo ? "text-gray-900" : "text-gray-400"} cursor-pointer`} 
-        onClick={() => handlePageChange(currentPage + 1)} 
+      <FaAngleRight
+        className={`bg-gray-200 text-3xl p-2 rounded-full ${
+          currentPage < pageNo ? "text-gray-900" : "text-gray-400"
+        } cursor-pointer`}
+        onClick={() => {
+          handlePageChange(currentPage + 1);
+  if(totalItem>ePage){
+    setSPage(sPage + 9);
+            setEPage(ePage + 9);
+  }
+        
+       
+        }}
       />
     </div>
   );
